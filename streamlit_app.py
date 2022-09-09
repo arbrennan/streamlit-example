1c104d23f342a1cbc14f6c8df58b830e8256a0f7
Ryan B <rbren90@gmail.com>
2022-09-09 16:29:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,34 +5,21 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome to PB Demand Prediction!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+Input the variables, click a button, and get the outputs. 
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+st.title('Offer Demand Predictor')
+st.header('Enter the characteristics of the offer:')
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+start_date = st.date_input('Offer start date')
+end_date = st.date_input('Offer end date')
+share_price = st.number_input('Offer price in pence')
+discount = st.number_input('Discount as a decimal', min_value=0.01, max_value=0.99, value=0.05)
 
-    points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if st.button('Predict Demand'):
+    price = predict(carat, cut, color, clarity, depth, table, x, y, z)
+    st.success(f'The predicted demand is ${price[0]:.2f} USD')
